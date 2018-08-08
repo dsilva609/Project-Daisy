cf95286ee9474cd821294987bd76a92b82db71e9
Dennis D. Silva <dsilva609@gmail.com>
2018-08-07 23:55:42 -0400

    added call to api for showcased items

---
+++ Project-Daisy/Project-Daisy.Model/ShowcasedItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Project_Daisy.Model
+{
+    public class ShowcasedItem
+    {
+        public string Artist { get; set; }
+        public int MediaType { get; set; }
+        public string Style { get; set; }
+        public int Speed { get; set; }
+        public int Size { get; set; }
+        public string RecordLabel { get; set; }
+        public int DiscogsID { get; set; }
+        public int ID { get; set; }
+        public string UserID { get; set; }
+        public string Title { get; set; }
+        public int UserNum { get; set; }
+        public string Genre { get; set; }
+        public object Language { get; set; }
+        public string Notes { get; set; }
+        public string ImageUrl { get; set; }
+        public int YearReleased { get; set; }
+        public bool IsPhysical { get; set; }
+        public bool IsNew { get; set; }
+        public string LocationPurchased { get; set; }
+        public DateTime DatePurchased { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public DateTime LastCompleted { get; set; }
+        public int CompletionStatus { get; set; }
+        public DateTime DateStarted { get; set; }
+        public DateTime DateCompleted { get; set; }
+        public bool CheckedOut { get; set; }
+        public int TimesCompleted { get; set; }
+        public object Category { get; set; }
+        public string CountryOfOrigin { get; set; }
+        public string CountryPurchased { get; set; }
+        public bool IsShowcased { get; set; }
+        public bool IsQueued { get; set; }
+        public int QueueRank { get; set; }
+    }
+}
--- Project-Daisy/Project-Daisy/Project-Daisy/Views/AboutPage.xaml.cs
+++ Project-Daisy/Project-Daisy/Project-Daisy/Views/AboutPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Project_Daisy.Models;
+﻿using Newtonsoft.Json;
+using Project_Daisy.Model;
+using RestSharp;
 using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -11,14 +13,21 @@ namespace Project_Daisy.Views
         public AboutPage()
         {
             InitializeComponent();
-            itemView.ItemsSource = new List<Item>
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            var client = new RestClient("http://cinderellacoreapi.azurewebsites.net");
+            var request = new RestRequest("api/showcase/getshowcaseditems/1", Method.GET);
+            request.AddHeader("Authorization", "p6UUHC2+wuS-YAaS");
+            request.AddHeader("Accept", "appliction/json");
+            var response = client.Execute(request);
+            if (response.IsSuccessful)
             {
-                new Item
-                {
-                    Description = "test 1",
-                },
-                new Item{Description = "test 2"}
-            };
+                var items = JsonConvert.DeserializeObject<List<ShowcasedItem>>(response.Content);
+                itemView.ItemsSource = items;
+            }
         }
     }
 }
