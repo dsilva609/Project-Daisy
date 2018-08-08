a529fb38d5ff64b564c81c8186ef357f7f31f8f0
Dennis D. Silva <dsilva609@gmail.com>
2018-08-07 23:14:20 -0400

    added list view

--- Project-Daisy/Project-Daisy/Project-Daisy/Views/AboutPage.xaml.cs
+++ Project-Daisy/Project-Daisy/Project-Daisy/Views/AboutPage.xaml.cs
@@ -1,16 +1,24 @@
-﻿using System;
-
+﻿using Project_Daisy.Models;
+using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Project_Daisy.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class AboutPage : ContentPage
-	{
-		public AboutPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+            itemView.ItemsSource = new List<Item>
+            {
+                new Item
+                {
+                    Description = "test 1",
+                },
+                new Item{Description = "test 2"}
+            };
+        }
+    }
 }
